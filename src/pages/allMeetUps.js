2177f6eeb546bffc43af03c252e1d9a47b85269b
Tonny221 <anthonny14gabriell@gmail.com>
2021-08-28 12:22:06 -0400

    list array

--- src/pages/allMeetUps.js
+++ src/pages/allMeetUps.js
@@ -1,6 +1,32 @@
-function AllMeetupsPage(){
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'Marseille',
+        image: 'https://viagemeturismo.abril.com.br/wp-content/uploads/2016/12/10-9c.jpg',
+        address: 'Italy',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae neque fermentum ante eleifend convallis.'
+    },
+    {
+        id: 'm2',
+        title: 'Tokyo',
+        image: 'https://coisasdojapao.com/wp-content/uploads/2017/03/Tokyo-Main-Image.jpg',
+        address: 'Japan',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae neque fermentum ante eleifend convallis.'
+    }
+];
+
+function AllMeetupsPage() {
     return (
-        <div>All meet ups page</div>
+        <section>
+            <h1>Cool cities</h1>
+            <ul>
+                {DUMMY_DATA.map((meetup) => {
+                    return (
+                        <li key={meetup.id}>{meetup.title}</li>
+                    );
+                })}
+            </ul>
+        </section>
     );
 }
 
