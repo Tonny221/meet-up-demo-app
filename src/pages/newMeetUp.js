537f2fe1a5b412099e37f90d60c8b9fe286acd9c
Tonny221 <anthonny14gabriell@gmail.com>
2021-09-03 09:41:06 -0400

    http post request

--- src/pages/newMeetUp.js
+++ src/pages/newMeetUp.js
@@ -1,11 +1,23 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
-function NewMeetupPage(){
-    return(
+function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://boxwood-well-317919-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
+    return (
         <section>
             <h1>New Meetup Page</h1>
-            <NewMeetupForm />
-        </section>   
+            <NewMeetupForm onAddNewMeetup={addMeetupHandler} />
+        </section>
     );
 }
 
