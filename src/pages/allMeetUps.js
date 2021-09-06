0910aaa7ee40b9fc8c60d8573a950587bc1ed65d
Tonny221 <anthonny14gabriell@gmail.com>
2021-09-06 11:56:13 -0400

    correction

--- src/pages/allMeetUps.js
+++ src/pages/allMeetUps.js
@@ -1,53 +1,50 @@
-import MeetupList from '../components/meetups/MeetupList'
-
 import { useState, useEffect } from 'react';
 
+import MeetupList from '../components/meetups/MeetupList';
+
 function AllMeetupsPage() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [loadedMeetups, setLoadedMeetups] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    useEffect(
-        () => {
-            setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://boxwood-well-317919-default-rtdb.firebaseio.com/meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
 
-            fetch('https://boxwood-well-317919-default-rtdb.firebaseio.com/meetups.json').then(
-                (response) => {
-                    response.json();
-                }
-            ).then(
-                (data) => {
-                    const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
 
-                    for (const key in data ) {
-                        const meetup = {
-                            id: key,
-                            ...data[key]
-                        };
+          meetups.push(meetup);
+        }
 
-                        meetups.push(meetup)
-                    }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
-                    setIsLoading(false);
-                    setLoadedMeetups(meetups);
-                }
-            );
-        }, []
-    );
-
-    if (isLoading) {
-        return (
-            <section>
-                <p>Loading...</p>
-            </section>
-        );
-    }
-
+  if (isLoading) {
     return (
-        <section>
-            <h1>Cool cities</h1>
-            <MeetupList meetups={loadedMeetups} />
-        </section>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
