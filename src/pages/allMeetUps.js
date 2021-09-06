c25092b4215892b0b47edf6a6057869e23c7af4a
Tonny221 <anthonny14gabriell@gmail.com>
2021-09-06 10:01:02 -0400

    fetching data

--- src/pages/allMeetUps.js
+++ src/pages/allMeetUps.js
@@ -1,27 +1,34 @@
 import MeetupList from '../components/meetups/MeetupList'
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'Marseille',
-        image: 'https://viagemeturismo.abril.com.br/wp-content/uploads/2016/12/10-9c.jpg',
-        address: 'Italy',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae neque fermentum ante eleifend convallis.'
-    },
-    {
-        id: 'm2',
-        title: 'Tokyo',
-        image: 'https://coisasdojapao.com/wp-content/uploads/2017/03/Tokyo-Main-Image.jpg',
-        address: 'Japan',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae neque fermentum ante eleifend convallis.'
-    }
-];
+import { useState } from 'react';
 
 function AllMeetupsPage() {
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ loadedMeetups, setLoadedMeetups ] = useState([])
+    
+    fetch('https://boxwood-well-317919-default-rtdb.firebaseio.com/meetups.json').then(
+        response => {
+            response.json();
+        }
+    ).then(
+        data => {
+            setIsLoading(false);
+            setLoadedMeetups(data);
+        }
+    );
+
+    if(isLoading){
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
     return (
         <section>
             <h1>Cool cities</h1>
-            <MeetupList meetups={DUMMY_DATA} />   
+            <MeetupList meetups={loadedMeetups} />
         </section>
     );
 }
